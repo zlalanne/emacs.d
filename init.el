c08fcea21ba67866aff5782cfe52accc61887af1
Zack Lalanne <zack.lalanne@gmail.com>
2017-06-09 07:43:59 -0500

    Removing extra require statements

--- init.el
+++ init.el
@@ -4,9 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Sync/20_29__House/23__Projects/23_03__Ethernet_Wiring/notes.org"))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
