a2b5166a9b05aa093b0e2b8823bbd035ac23273c
Zack Lalanne <zack.lalanne@ti.com>
2017-06-09 10:48:16 -0500

    Adding support for yaml/ansible

--- init.el
+++ init.el
@@ -11,7 +11,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+    (yaml-mode ansible use-package solarized-theme ox-twbs org-bullets neotree markdown-mode helm evil diff-hl company all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
