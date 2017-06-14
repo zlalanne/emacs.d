05c473ee0ce4696e5bb87474c6db21b8b75a1e8f
Zack Lalanne <zack.lalanne@gmail.com>
2017-06-14 11:01:21 -0500

    Fixing syntax errors in emacs configuration files

--- init.el
+++ init.el
@@ -1,23 +1,3 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (helm-projectile evil-magit yaml-mode ansible use-package solarized-theme ox-twbs org-bullets neotree markdown-mode helm evil diff-hl company all-the-icons)))
- '(projectile-mode t nil (projectile)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
